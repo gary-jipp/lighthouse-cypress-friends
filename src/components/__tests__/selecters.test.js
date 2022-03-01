8b867c732d74f133f827eb09d9ce3b96851d960b
Gary J <github@accu.ca>
2022-02-28 19:43:55 -0800

    Update

---
+++ src/components/__tests__/selecters.test.js
@@ -0,0 +1,33 @@
+const { getFriendCount, removeFriend, getFriendNames } = require("helpers");
+
+const data = [
+  { name: "Betty White", uid: "1" },
+  { name: "Freddy Mercury", uid: "2" },
+  { name: "James Holden", uid: "3" },
+  { name: "Tom Cruise", uid: "4" },
+];
+
+describe('Selector tests', () => {
+
+  test("returns count of friends", () => {
+    const count = getFriendCount(data);
+
+    expect(count).toBe(4);
+  });
+
+  it("get correct friends names", () => {
+    const names = getFriendNames(data);
+    expect(names).toContain("Betty White");
+  });
+
+  it("removes unwanted friends", () => {
+    const tom = { name: "Tom Cruise", uid: "4" };
+    expect(data).toContainEqual(tom);
+
+    const bestFriends = removeFriend(data, "4");
+    expect(bestFriends.length).toBe(3);
+
+    expect(bestFriends).not.toContainEqual(tom);
+  });
+
+});
