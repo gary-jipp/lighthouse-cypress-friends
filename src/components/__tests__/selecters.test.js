bd20dce9d9f8ea4e84203f203a9a64dad1bf14be
Gary J <github@accu.ca>
2022-03-28 21:34:56 -0700

    Update

--- src/components/__tests__/selecters.test.js
+++ src/components/__tests__/selecters.test.js
@@ -8,40 +8,48 @@ const data = [
 ];
 
 describe('Selector tests', () => {
+  //---------------------------------------
 
   test("returns count of friends", () => {
     const count = getFriendCount(data);
     expect(count).toBe(4);
   });
 
-  //Coverage Check
-  test("returns -1 no friends", () => {
+  // Coverage Check
+  test("returns empty array if no friends", () => {
     const count = getFriendCount(null);
     expect(count).toBe(-1);
   });
-
-
+  
+  //---------------------------------------
   it("get correct friends names", () => {
     const names = getFriendNames(data);
-    expect(names.length).toBe(4);
     expect(names).toContain("Betty White");
+    expect(names.length).toBe(4);
   });
 
+  // Coverage Check
+  test("returns empty names if null friends", () => {
+    const result = getFriendNames(null);
+    expect(result.length).toBe(0);
+  });
+
+  //---------------------------------------
   it("removes unwanted friends", () => {
     const tom = { name: "Tom Cruise", uid: "4" };
     expect(data).toContainEqual(tom);
 
     const bestFriends = removeFriend(data, "4");
     expect(bestFriends.length).toBe(3);
-
     expect(bestFriends).not.toContainEqual(tom);
   });
 
-
-  // Coverage Check!!
-  test("returns empty array if no friends", () => {
+  // Coverage Check
+  test("returns empty on remove if null friends", () => {
     const result = removeFriend(null);
     expect(result.length).toBe(0);
   });
+
+  //---------------------------------------
 
 });
